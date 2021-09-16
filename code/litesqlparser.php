2b2f31a91b5d2a222f8e13be62eb648b5fe7780a
Oliver Jones <olliejones@gmail.com>
2021-09-16 17:02:43 -0400

    query monitor log with adequately tokenized queries.

--- code/litesqlparser.php
+++ code/litesqlparser.php
@@ -2,39 +2,142 @@
 
 /**
  * Light SQL Parser Class
- * @author Marco Cesarato <cesarato.developer@gmail.com>
+ * @author Marco Cesarato <cesarato.developer@gmail.com>, Ollie JOnes <olliejones@gmail.com>
  * @copyright Copyright (c) 2021
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
  * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
  */
-class LightSQLParser
-{
+class LightSQLParser {
 	// Public
-	public $query = '';
+	protected static $connectors = array(
+		'OR',
+		'AND',
+		'ON',
+		'LIMIT',
+		'WHERE',
+		'JOIN',
+		'GROUP',
+		'ORDER',
+		'OPTION',
+		'LEFT',
+		'INNER',
+		'RIGHT',
+		'OUTER',
+		'SET',
+		'HAVING',
+		'VALUES',
+		'SELECT',
+		'\(',
+		'\)'
+	);
 	// Private
-	protected static $connectors = array('OR', 'AND', 'ON', 'LIMIT', 'WHERE', 'JOIN', 'GROUP', 'ORDER', 'OPTION', 'LEFT', 'INNER', 'RIGHT', 'OUTER', 'SET', 'HAVING', 'VALUES', 'SELECT', '\(', '\)');
 	protected static $connectors_imploded = '';
+	public $query = '';
 	protected $queries = [];
-	protected $tokenRe = <<<'END_OF_REGEX'
-/
-  [A-Za-z_.]+\(.*?\)+   # Match FUNCTION(...)
-  |\(.*?\)+             # Match grouped items
-  |"(?:[^"]|\"|"")*"+   # Match double quotes
-  |'[^'](?:|\'|'')*'+   #   and single quotes
-  |`(?:[^`]|``)*`+      #   and backticks
-  |[^ ,]+
-  |,
-/x
-END_OF_REGEX;
 
 	/**
 	 * Constructor
 	 */
-	public function __construct($query = '') {
-		$this->setQuery($query);
-		if (empty(self::$connectors_imploded)) {
-			self::$connectors_imploded = implode('|', self::$connectors);
+	public function __construct( $query = '' ) {
+		$this->setQuery( $query );
+		if ( empty( self::$connectors_imploded ) ) {
+			self::$connectors_imploded = implode( '|', self::$connectors );
 		}
+	}
+
+	/**
+	 * Get Query fields (at the moment only SELECT/INSERT/UPDATE)
+	 *
+	 * @param $query
+	 *
+	 * @return array
+	 */
+	public function getFields() {
+		$fields  = array();
+		$queries = $this->getAllQueries();
+		foreach ( $queries as $query ) {
+			$method = $this->getMethod();
+			switch ( $method ) {
+				case 'SELECT':
+					preg_match( '#SELECT[\s]+([\S\s]*)[\s]+FROM#i', $query, $matches );
+					if ( ! empty( $matches[1] ) ) {
+						$match = trim( $matches[1] );
+						$match = explode( ',', $match );
+						foreach ( $match as $field ) {
+							$field    = preg_replace( '#([\s]+(AS[\s]+)?[\w.]+)#i', '', trim( $field ) );
+							$fields[] = $field;
+						}
+					}
+					break;
+				case 'INSERT':
+					preg_match( '#INSERT[\s]+INTO[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)\(([\S\s]*)\)[\s]+VALUES#i', $query, $matches );
+					if ( ! empty( $matches[4] ) ) {
+						$match = trim( $matches[4] );
+						$match = explode( ',', $match );
+						foreach ( $match as $field ) {
+							$field    = preg_replace( '#([\s]+(AS[\s]+)?[\w.]+)#i', '', trim( $field ) );
+							$fields[] = $field;
+						}
+					} else {
+						preg_match( '#INSERT[\s]+INTO[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)SET([\S\s]*)([;])?#i', $query, $matches );
+						if ( ! empty( $matches[4] ) ) {
+							$match = trim( $matches[4] );
+							$match = explode( ',', $match );
+							foreach ( $match as $field ) {
+								$field    = preg_replace( '#([\s]*=[\s]*[\S\s]+)#i', '', trim( $field ) );
+								$fields[] = $field;
+							}
+						}
+					}
+					break;
+				case 'UPDATE':
+					preg_match( '#UPDATE[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)SET([\S\s]*)([\s]+WHERE|[;])?#i', $query, $matches );
+					if ( ! empty( $matches[4] ) ) {
+						$match = trim( $matches[4] );
+						$match = explode( ',', $match );
+						foreach ( $match as $field ) {
+							$field    = preg_replace( '#([\s]*=[\s]*[\S\s]+)#i', '', trim( $field ) );
+							$fields[] = $field;
+						}
+					}
+					break;
+				case 'CREATE TABLE':
+					preg_match( '#CREATE[\s]+TABLE[\s]+\w+[\s]+\(([\S\s]*)\)#i', $query, $matches );
+					if ( ! empty( $matches[1] ) ) {
+						$match = trim( $matches[1] );
+						$match = explode( ',', $match );
+						foreach ( $match as $_field ) {
+							preg_match( '#^w+#', trim( $_field ), $field );
+							if ( ! empty( $field[0] ) ) {
+								$fields[] = $field[0];
+							}
+						}
+					}
+					break;
+			}
+		}
+
+		return array_unique( $fields );
+	}
+
+	/**
+	 * Get all queries
+	 * @return array
+	 */
+	public function getAllQueries() {
+		if ( empty( $this->queries ) ) {
+			// TODO: fix issues when on a subquery exists a UNION expression
+			$query   = $this->getQuery();
+			$query   = preg_replace( '#/\*[\s\S]*?\*/#', '', $query );
+			$query   = preg_replace( '#;(?:(?<=["\'];)|(?=["\']))#', '', $query );
+			$query   = preg_replace( '#[\s]*UNION([\s]+ALL)?[\s]*#', ';', $query );
+			$queries = explode( ';', $query );
+			foreach ( $queries as $key => $query ) {
+				$this->queries[ $key ] = str_replace( array( '`', '"', "'" ), '', $query );
+			}
+		}
+
+		return $this->queries;
 	}
 
 	/**
@@ -48,29 +151,11 @@ END_OF_REGEX;
 	/**
 	 * Set SQL Query string
 	 */
-	public function setQuery($query) {
-		$this->query = $query;
+	public function setQuery( $query ) {
+		$this->query   = $query;
 		$this->queries = [];
-		return $this;
-	}
 
-	/**
-	 * Get all queries
-	 * @return array
-	 */
-	public function getAllQueries() {
-		if(empty($this->queries)) {
-			// TODO: fix issues when on a subquery exists a UNION expression
-			$query = $this->getQuery();
-			$query = preg_replace('#/\*[\s\S]*?\*/#', '', $query);
-			$query = preg_replace('#;(?:(?<=["\'];)|(?=["\']))#', '', $query);
-			$query = preg_replace('#[\s]*UNION([\s]+ALL)?[\s]*#', ';', $query);
-			$queries = explode(';', $query);
-			foreach ($queries as $key => $query) {
-				$this->queries[$key] = str_replace(array('`', '"', "'"), '', $query);
-			}
-		}
-		return $this->queries;
+		return $this;
 	}
 
 	/**
@@ -78,99 +163,46 @@ END_OF_REGEX;
 	 * @return string
 	 */
 	public function getMethod() {
-		$methods = array('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'RENAME', 'SHOW', 'SET', 'DROP', 'CREATE INDEX', 'CREATE TABLE', 'EXPLAIN', 'DESCRIBE', 'TRUNCATE', 'ALTER');
+		$methods = array(
+			'SELECT',
+			'INSERT',
+			'UPDATE',
+			'DELETE',
+			'RENAME',
+			'SHOW',
+			'SET',
+			'DROP',
+			'CREATE INDEX',
+			'CREATE TABLE',
+			'EXPLAIN',
+			'DESCRIBE',
+			'TRUNCATE',
+			'ALTER'
+		);
 		$queries = $this->getAllQueries();
-		foreach ($queries as $query) {
-			foreach ($methods as $method) {
-				$_method = str_replace(' ', '[\s]+', $method);
-				if (preg_match('#^[\s]*' . $_method . '[\s]+#i', $query)) {
+		foreach ( $queries as $query ) {
+			foreach ( $methods as $method ) {
+				$_method = str_replace( ' ', '[\s]+', $method );
+				if ( preg_match( '#^[\s]*' . $_method . '[\s]+#i', $query ) ) {
 					return $method;
 				}
 			}
 		}
+
 		return '';
 	}
 
 	/**
-	 * Get Query fields (at the moment only SELECT/INSERT/UPDATE)
-	 * @param $query
-	 * @return array
-	 */
-	public function getFields() {
-		$fields = array();
-		$queries = $this->getAllQueries();
-		foreach ($queries as $query) {
-			$method = $this->getMethod();
-			switch ($method) {
-				case 'SELECT':
-					preg_match('#SELECT[\s]+([\S\s]*)[\s]+FROM#i', $query, $matches);
-					if (!empty($matches[1])) {
-						$match = trim($matches[1]);
-						$match = explode(',', $match);
-						foreach ($match as $field) {
-							$field = preg_replace('#([\s]+(AS[\s]+)?[\w.]+)#i', '', trim($field));
-							$fields[] = $field;
-						}
-					}
-					break;
-				case 'INSERT':
-					preg_match('#INSERT[\s]+INTO[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)\(([\S\s]*)\)[\s]+VALUES#i', $query, $matches);
-					if (!empty($matches[4])) {
-						$match = trim($matches[4]);
-						$match = explode(',', $match);
-						foreach ($match as $field) {
-							$field = preg_replace('#([\s]+(AS[\s]+)?[\w.]+)#i', '', trim($field));
-							$fields[] = $field;
-						}
-					} else {
-						preg_match('#INSERT[\s]+INTO[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)SET([\S\s]*)([;])?#i', $query, $matches);
-						if (!empty($matches[4])) {
-							$match = trim($matches[4]);
-							$match = explode(',', $match);
-							foreach ($match as $field) {
-								$field = preg_replace('#([\s]*=[\s]*[\S\s]+)#i', '', trim($field));
-								$fields[] = $field;
-							}
-						}
-					}
-					break;
-				case 'UPDATE':
-					preg_match('#UPDATE[\s]+([\w.]+([\s]+(AS[\s]+)?[\w.]+)?[\s]*)SET([\S\s]*)([\s]+WHERE|[;])?#i', $query, $matches);
-					if (!empty($matches[4])) {
-						$match = trim($matches[4]);
-						$match = explode(',', $match);
-						foreach ($match as $field) {
-							$field = preg_replace('#([\s]*=[\s]*[\S\s]+)#i', '', trim($field));
-							$fields[] = $field;
-						}
-					}
-					break;
-				case 'CREATE TABLE':
-					preg_match('#CREATE[\s]+TABLE[\s]+\w+[\s]+\(([\S\s]*)\)#i', $query, $matches);
-					if (!empty($matches[1])) {
-						$match = trim($matches[1]);
-						$match = explode(',', $match);
-						foreach ($match as $_field) {
-							preg_match('#^w+#', trim($_field), $field);
-							if (!empty($field[0])) {
-								$fields[] = $field[0];
-							}
-						}
-					}
-					break;
-			}
-		}
-		return array_unique($fields);
-	}
-
-	/**
 	 * Get SQL Query First Table
+	 *
 	 * @param $query
+	 *
 	 * @return string
 	 */
 	public function getTable() {
 		$tables = $this->getAllTables();
-		return (isset($tables[0])) ? $tables[0] : null;
+
+		return ( isset( $tables[0] ) ) ? $tables[0] : null;
 	}
 
 	/**
@@ -180,7 +212,7 @@ END_OF_REGEX;
 	function getAllTables() {
 		$results = array();
 		$queries = $this->getAllQueries();
-		foreach ($queries as $query) {
+		foreach ( $queries as $query ) {
 			$patterns = array(
 				'#[\s]+FROM[\s]+(([\s]*(?!' . self::$connectors_imploded . ')[\w]+([\s]+(AS[\s]+)?(?!' . self::$connectors_imploded . ')[\w]+)?[\s]*[,]?)+)#i',
 				'#[\s]*INSERT[\s]+INTO[\s]+([\w]+)#i',
@@ -189,18 +221,19 @@ END_OF_REGEX;
 				'#[\s]+TABLE[\s]+([\w]+)#i',
 				'#[\s]+TABLESPACE[\s]+([\w]+)#i',
 			);
-			foreach ($patterns as $pattern) {
-				preg_match_all($pattern, $query, $matches, PREG_SET_ORDER);
-				foreach ($matches as $val) {
-					$tables = explode(',', $val[1]);
-					foreach ($tables as $table) {
-						$table = trim(preg_replace('#[\s]+(AS[\s]+)[\w.]+#i', '', $table));
+			foreach ( $patterns as $pattern ) {
+				preg_match_all( $pattern, $query, $matches, PREG_SET_ORDER );
+				foreach ( $matches as $val ) {
+					$tables = explode( ',', $val[1] );
+					foreach ( $tables as $table ) {
+						$table     = trim( preg_replace( '#[\s]+(AS[\s]+)[\w.]+#i', '', $table ) );
 						$results[] = $table;
 					}
 				}
 			}
 		}
-		return array_unique($results);
+
+		return array_unique( $results );
 	}
 
 	/**
@@ -210,17 +243,18 @@ END_OF_REGEX;
 	function getJoinTables() {
 		$results = array();
 		$queries = $this->getAllQueries();
-		foreach ($queries as $query) {
-			preg_match_all('#[\s]+JOIN[\s]+([\w]+)#i', $query, $matches, PREG_SET_ORDER);
-			foreach ($matches as $val) {
-				$tables = explode(',', $val[1]);
-				foreach ($tables as $table) {
-					$table = trim(preg_replace('#[\s]+(AS[\s]+)[\w.]+#i', '', $table));
+		foreach ( $queries as $query ) {
+			preg_match_all( '#[\s]+JOIN[\s]+([\w]+)#i', $query, $matches, PREG_SET_ORDER );
+			foreach ( $matches as $val ) {
+				$tables = explode( ',', $val[1] );
+				foreach ( $tables as $table ) {
+					$table     = trim( preg_replace( '#[\s]+(AS[\s]+)[\w.]+#i', '', $table ) );
 					$results[] = $table;
 				}
 			}
 		}
-		return array_unique($results);
+
+		return array_unique( $results );
 	}
 
 	/**
@@ -229,12 +263,13 @@ END_OF_REGEX;
 	 */
 	function hasJoin() {
 		$queries = $this->getAllQueries();
-		foreach ($queries as $query) {
-			preg_match('#[\s]+JOIN[\s]+([\w]+)#i', $query, $matches);
-			if(!empty($matches[1])) {
+		foreach ( $queries as $query ) {
+			preg_match( '#[\s]+JOIN[\s]+([\w]+)#i', $query, $matches );
+			if ( ! empty( $matches[1] ) ) {
 				return true;
 			}
 		}
+
 		return false;
 	}
 
@@ -244,10 +279,11 @@ END_OF_REGEX;
 	 */
 	function hasSubQuery() {
 		$query = $this->getQuery();
-		preg_match('#\([\s]*(SELECT[^)]+)\)#i', $query, $matches);
-		if(!empty($matches[1])) {
+		preg_match( '#\([\s]*(SELECT[^)]+)\)#i', $query, $matches );
+		if ( ! empty( $matches[1] ) ) {
 			return true;
 		}
+
 		return false;
 	}
 
@@ -257,17 +293,59 @@ END_OF_REGEX;
 	 */
 	function getSubQueries() {
 		$results = array();
-		$query = $this->getQuery();
-		preg_match_all('#\([\s]*(SELECT[^)]+)\)#i', $query, $matches, PREG_SET_ORDER);
-		foreach ($matches as $match) {
+		$query   = $this->getQuery();
+		preg_match_all( '#\([\s]*(SELECT[^)]+)\)#i', $query, $matches, PREG_SET_ORDER );
+		foreach ( $matches as $match ) {
 			$results[] = $match[0];
 		}
-		return array_unique($results);
+
+		return array_unique( $results );
 	}
 
-	function getFingerprint () {
+	function getFingerprint() {
 		$query = $this->getQuery();
-		$tokens = preg_match_all($this->tokenRe, $query);
-	}
 
+		$result = $query;
+		/* backticks */
+		$result = preg_replace( '/\`([^\`]+)\`/', '$1', $result );
+
+		/* take off LIMIT and OFFSET */
+		$limitp      = strripos( $result, ' LIMIT ' );
+		$offsetp     = strripos( $result, ' OFFSET ' );
+		$limitClause = '';
+		if ( $limitp > 0 || $offsetp > 0 ) {
+			$limitp      = $limitp === false ? PHP_INT_MAX : $limitp;
+			$offsetp     = $offsetp === false ? PHP_INT_MAX : $offsetp;
+			$p           = min( $limitp, $offsetp );
+			$limitClause = substr( $result, $p );
+			$result      = substr( $result, 0, $p );
+		}
+
+		/* date and time constants */
+		$result = preg_replace( '/\'\d\d\d\d-\d\d-\d\d \d\d:\d\d:\dd/', '?datetime?', $result );
+		$result = preg_replace( '/\'\d\d\d\d-\d\d-\d\d/', '?date?', $result );
+		$result = preg_replace( '/\'[0-9]{10}\'/', '?t?', $result );
+
+		/* integers */
+		$result = preg_replace( '/= +\d+/', '= ?i?', $result );
+		$result = preg_replace( '/= +\'\d+\'/', '= ?qi?', $result );
+		$result = preg_replace( '/IN +\( *\d+} *\)/', 'IN (?i?)', $result );
+		$result = preg_replace( '/IN +\( *\d+ *, *\d+ *\)/', 'IN (?i?, ?i?)', $result );
+		$result = preg_replace( '/IN +\( *(\d+, *){2,9}(\d+ *\))/', 'IN (?ilist?)', $result );
+		$result = preg_replace( '/IN +\( *(\d+, *){10,}(\d+ *\))/', 'IN (?ilonglist?)', $result );
+		/* unadorned integers, except in table names */
+		$result = preg_replace( '/(?:[^_])\d+/', '?i?', $result );
+
+		/* quoted strings, with escapes processed correctly */
+		$result = preg_replace( '/\'(?:.*?[^\\\\])??(?:(?:\\\\\\\\)+)?\'/', '?s?', $result );
+
+		/* quoted strings, with escapes processed correctly */
+		$result = preg_replace( "/(INSERT +[^\\(]+\\([^\\)]+\\) *VALUES )(?:.{150,}+)/", '$1 (?valuelist?)', $result );
+
+		$result = strlen( $limitClause ) > 0 ? $result . ' ' . $limitClause : $result;
+		/* extra white space */
+		$result = preg_replace( '/\s+/', ' ', $result );
+
+		return $result;
+	}
 }
--- code/querymon.php
+++ code/querymon.php
@@ -8,18 +8,22 @@ if ( ! defined( 'SAVEQUERIES' ) ) {
 add_action( 'shutdown', 'imfsMonitorGather', 99 );
 
 function imfsMonitorGather() {
-	$time_threshold = 0.00001;
-	$queryLogSizeThreshold = 1024 * 1024 * 4;
-	$parser         = new LightSQLParser();
+	$time_threshold        = 0.00001;
+	$queryLogSizeThreshold = 1024 * 1024;
+	$parser                = new LightSQLParser();
 	global $wpdb;
 
 	$queryLogOverflowing = false;
 	$queryLog            = get_transient( index_wp_mysql_for_speed_monitor . 'Log' );
 	if ( ! $queryLog ) {
-		$queryLog = array();
+		$queryLog          = (object) array();
+		$queryLog->queries = array();
+		$queryLog->count   = 1;
 	} else {
 		$queryLogOverflowing = strlen( $queryLog ) > $queryLogSizeThreshold;
 		$queryLog            = json_decode( $queryLog );
+		$queryLog->queries   = (array) $queryLog->queries;
+		$queryLog->count ++;
 	}
 
 	$queries = array();
@@ -47,33 +51,36 @@ function imfsMonitorGather() {
 	$keys   = array();
 	foreach ( $queries as $q ) {
 		try {
-			$caller = '';
-			$query  = $q[0];
+			$query = $q[0];
 			$parser->setQuery( $query );
 			$method = $parser->getMethod();
 			/* don't analyze SHOW VARIABLES and other SHOW commands */
 			if ( $method != 'SHOW' ) {
-				/* don't use ANALYZE on queries that change data, because it will actually change the data. */
-				$explainer       = $method == 'SELECT' ? 'ANALYZE' : 'EXPLAIN';
-				$explain         = $explainer . ' ' . $query;
-				$explanations    = $wpdb->get_results( $explain );
 				$logEntry        = (object) [];
-				$qid             = substr( hash( 'md5', $query, false ), 0, 12 );
-				$logEntry->q     = $query;
+				$fingerprint     = $parser->getFingerprint();
+				$qid             = substr( hash( 'md5', $fingerprint, false ), 0, 16 );
+				$logEntry->f     = $fingerprint;
 				$logEntry->t     = $q[1];
 				$logEntry->n     = 1;
-				$logEntry->admin = !!is_admin();
-				$logEntry->exp   = $explanations;
-				if ( $queryLog->{$qid} ) {
-					$queryLog->{$qid}->n     += 1;
-					$queryLog->{$qid}->t     += $logEntry->t;
-					$queryLog->{$qid}->admin += $logEntry->admin;
-					$queryLog->{$qid}->exp   += $logEntry->exp;
+				$logEntry->admin = ! ! is_admin();
+				if ( array_key_exists( $qid, $queryLog->queries ) ) {
+					$queryLog->queries[ $qid ]->n     += 1;
+					$queryLog->queries[ $qid ]->t     += $logEntry->t;
+					$queryLog->queries[ $qid ]->admin = $logEntry->admin;
 				} else if ( ! $queryLogOverflowing ) {
-					$queryLog[ $qid ] = $logEntry;
+					/* don't use ANALYZE on queries that change data, because it will actually change the data. */
+					$explainer                 = $method == 'SELECT' ? 'ANALYZE' : 'EXPLAIN';
+					$explain                   = $explainer . ' ' . $query;
+					$explanations              = $wpdb->get_results( $explain );
+					$logEntry->exp             = $explanations;
+					$logEntry->q               = $query;
+					$queryLog->queries[ $qid ] = $logEntry;
 				} else {
-					if ($queryLog->overflowed) $queryLog->overflowed += 1;
-					else $queryLog->overflowed = 1;
+					if ( $queryLog->overflowed ) {
+						$queryLog->overflowed += 1;
+					} else {
+						$queryLog->overflowed = 1;
+					}
 				}
 			}
 		} catch ( Exception $e ) {
