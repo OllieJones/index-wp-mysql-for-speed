e09d7f58a81d1fa585cd46b5d918f69ef8c5a9c6
Oliver Jones <olliejones@gmail.com>
2021-09-30 10:23:53 -0400

    table styling

--- code/assets/imfs.js
+++ code/assets/imfs.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
         })
         .DataTable({
             paging: false,
+            pagingType: "first_last_numbers",
+            lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
             searching: true,
             order: [[0, 'asc'], [2, 'asc']],
-            dom: 'Bfrtip',
+            orderClasses: false,
+            dom: 'iBfrtilp',
             buttons: [{extend: 'csv', text: 'Save as .csv'}],
         })
 })
